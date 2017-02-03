d90ac34e17be6aecadc2301f85c9686c94a1e56b
Csyk <balipaulcsik@gmail.com>
2017-02-03 11:05:00 +0100

    SiteController extended with /signup route

--- src/main/java/com/codecool/sherwoodbet/Controller/SiteController.java
+++ src/main/java/com/codecool/sherwoodbet/Controller/SiteController.java
@@ -2,7 +2,10 @@ package com.codecool.sherwoodbet.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by patrik on 2017.02.01..
@@ -13,5 +16,11 @@ public class SiteController {
     @RequestMapping("/")
     public String index (Model model){
         return "index";
+    }
+
+    @RequestMapping("/signup")
+    @ResponseBody
+    public String signup(@RequestBody() String JSON){
+        return "";
     }
 }

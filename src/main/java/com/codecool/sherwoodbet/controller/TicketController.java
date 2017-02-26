839983c959d878dc9ac4ca325274f30e36366a76
Csyk <balipaulcsik@gmail.com>
2017-02-26 18:58:53 +0100

    BET-36 create tables

--- src/main/java/com/codecool/sherwoodbet/controller/TicketController.java
+++ src/main/java/com/codecool/sherwoodbet/controller/TicketController.java
@@ -1,7 +1,5 @@
 package com.codecool.sherwoodbet.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,35 +16,75 @@ import java.util.Map;
 @Controller
 public class TicketController {
 
-    private static final Logger log = LoggerFactory.getLogger(SiteController.class);
+    private List<Map> tickets = new ArrayList<>();
+    private Map<String, Object> ticket = new HashMap<>();
+    private List<String> matches = new ArrayList<>();
 
-    @RequestMapping("/bet/api/playable_tickets")
-    @ResponseBody
-    public Map playableTickets() {
-        Map<String, List> response = new HashMap<>();
-        List<Map> tickets = new ArrayList<>();
-        Map<String, Object> ticket = new HashMap<>();
-        List<String> matches = new ArrayList<>();
+    private Map<String, Object> inticket = new HashMap<>();
+    private List<Map> aTicketMatches = new ArrayList<>();
+    private Map<String, Object> match = new HashMap<>();
+    private Map<String, Integer> bet = new HashMap<>();
+
+    private void creatingDatas() {
+        tickets.clear();
+        ticket.clear();
+        matches.clear();
+        inticket.clear();
+        aTicketMatches.clear();
+        match.clear();
+        bet.clear();
 
         matches.add("Békés - Tarhos");
         matches.add("Kamut - Murony");
+        matches.add("Brutál MÁV - bárki");
 
         ticket.put("title", "Demo");
         ticket.put("intro", "blabla");
         ticket.put("deadline", "tomorrow");
         ticket.put("matches", matches);
+        ticket.put("ticket_id", 1);
+
+        inticket.put("title", "Demo");
+        inticket.put("deadline", "tomorrow");
+        inticket.put("ticket_id", "1");
+        inticket.put("playable", "true");
+        inticket.put("intro", "blabla");
+        inticket.put("matches", aTicketMatches);
+        aTicketMatches.add(match);
+
+        match.put("id", 1);
+        match.put("match", "Békés - Tarhos");
+        match.put("venue", "Békés");
+        match.put("tournament", "edző meccs");
+        match.put("round_number", "1");
+        match.put("more_info", "asdfjlklé");
+        match.put("match_date", "today");
+        match.put("bet", bet);
+
+        bet.put("home", 0);
+        bet.put("draw", 0);
+        bet.put("away", 0);
+        bet.put("id", 1);
+        bet.put("match", 1);
+        bet.put("user_ticket", 1);
 
         tickets.add(ticket);
+    }
+
+    @RequestMapping("/bet/api/playable_tickets")
+    @ResponseBody
+    public Map playableTickets() {
+        Map<String, List> response = new HashMap<>();
+        creatingDatas();
 
         response.put("tickets", tickets);
-
         return response;
     }
 
     @RequestMapping("/bet/api/get_ticket")
     @ResponseBody
-    public String getTicket(@RequestParam String id) {
-        return id + " ticket";
+    public Map getTicket(@RequestParam int ticket) {
+        return inticket;
     }
 }
 

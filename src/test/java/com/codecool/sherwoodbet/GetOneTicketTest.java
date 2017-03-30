32b9c2e239492f3cb73388474fecd5e890b53688
Daniel Balogh <baloghdani88@gmail.com>
2017-03-30 13:41:41 +0200

    http status test for getoneticket method runs and fails

--- src/test/java/com/codecool/sherwoodbet/GetOneTicketTest.java
+++ src/test/java/com/codecool/sherwoodbet/GetOneTicketTest.java
@@ -2,12 +2,22 @@ package com.codecool.sherwoodbet;
 
 import com.codecool.sherwoodbet.controller.TicketController;
 import com.codecool.sherwoodbet.repository.TicketRepository;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 /**
  * Created by danielbalogh on 3/30/17.
@@ -17,10 +27,32 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class GetOneTicketTest {
 
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
     @Autowired
     TicketRepository ticketRepository;
 
-    @Mock
+    @Autowired
     TicketController ticketController;
 
+//    private final MockMvc mockMvc = standaloneSetup(new AddressController())
+//            .build();
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    public void validate_get_address() throws Exception {
+
+        mockMvc.perform(get("/bet/api/get_ticket"))
+                .andExpect(status().isOk());
+
+    }
 }
